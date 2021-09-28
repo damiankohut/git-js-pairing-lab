bdbc2f2ccb1f44718bd095e03ffc94f900ae1c1a
Damian <Your Github Email Address>
2021-09-28 19:10:57 +0000

    finished q1-5

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 //Code your solutions in this file
-damian and muhammed 
+//1
+function fiveToOneHundred(){
+for (let i= 5;i <= 50;i ++) 
+console.log(i)
+}
+
+
+//2
+function multiplesOfThree() {
+var number = 3;
+for (var number = 3; number <= 100; number++) {
+  if (number % 3 === 0) {
+    console.log(number);
+    }
+    }
+     }
+//3
+function multiplesOfThreeOrFive(){
+for (let i =1; i < 100; i++){
+    
+    if (i % 3 ===0 || i % 5 === 0) 
+        console.log(i)
+}
+}
+//4
+function untilNum(num) {
+    for (let i= 1;i <= num;i ++) 
+    console.log(i)
+}
+
+//5
+function multiply(num1,num2){
+    console.log(num1 * num2)
+}
+
+//6
+function add(num1, num2){
+    
+}
+
+
+
+//fiveToOneHundred()
+//multiplesOfThree()
+//multiplesOfThreeOrFive()
+//untilNum(42)
+//multiply(10,-5)
+add(5, 5)
